8b66fc8cb45d88932143d3d51d67d994cb60def7
ratchet <ratchet@congruency.local>
2026-07-05 20:31:38 -0600

    crank #12: guard Constants_patched.php define()s (config-loader can win)

--- checkouts/current/boot/Constants_patched.php
+++ checkouts/current/boot/Constants_patched.php
@@ -19,32 +19,32 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 <<<Contact Info>>>
 Steven Peterson,2234 4th Ave NW, Calgary, AB T2N 0N7 Canada. steven.peterson@shaw.ca
 */
-define("MYSQL_SERVER",'localhost');
-define("SERVER_LOGIN", 'db_uers');
-define("SERVER_PASSWORD", 'xxxx');
+if (!defined("MYSQL_SERVER")) define("MYSQL_SERVER",'localhost');
+if (!defined("SERVER_LOGIN")) define("SERVER_LOGIN", 'db_uers');
+if (!defined("SERVER_PASSWORD")) define("SERVER_PASSWORD", 'xxxx');
 
-define("MYSQL_AUTH_DATABASE", 'CONGRUENCY_USER');
-define("AUTHENTICATOR_LOGIN",'authenticator');
-define('AUTHENTICATOR_PASSWORD','auth995');
+if (!defined("MYSQL_AUTH_DATABASE")) define("MYSQL_AUTH_DATABASE", 'CONGRUENCY_USER');
+if (!defined("AUTHENTICATOR_LOGIN")) define("AUTHENTICATOR_LOGIN",'authenticator');
+if (!defined('AUTHENTICATOR_PASSWORD')) define('AUTHENTICATOR_PASSWORD','auth995');
 
-define("MYSQL_CONT_DATABASE", 'CONGRUENCY_DOCUMENT');
-define("CONTENT_LOGIN",'content');
-define('CONTENT_PASSWORD','cont995');
+if (!defined("MYSQL_CONT_DATABASE")) define("MYSQL_CONT_DATABASE", 'CONGRUENCY_DOCUMENT');
+if (!defined("CONTENT_LOGIN")) define("CONTENT_LOGIN",'content');
+if (!defined('CONTENT_PASSWORD')) define('CONTENT_PASSWORD','cont995');
 
 /* These were referenced throughout the store/order/form/auth modules but never
    defined here, so those subsystems fatal on construction (undefined constants
    are fatal on PHP 8). Placeholder credentials; set per install. */
-define("MYSQL_STORE_DATABASE", 'CONGRUENCY_STORE');
-define("STORE_LOGIN", 'store');
-define('STORE_PASSWORD', 'store995');
+if (!defined("MYSQL_STORE_DATABASE")) define("MYSQL_STORE_DATABASE", 'CONGRUENCY_STORE');
+if (!defined("STORE_LOGIN")) define("STORE_LOGIN", 'store');
+if (!defined('STORE_PASSWORD')) define('STORE_PASSWORD', 'store995');
 
-define("MYSQL_ORDER_DATABASE", 'CONGRUENCY_ORDER');
-define("ORDER_LOGIN", 'orderer');
-define('ORDER_PASSWORD', 'order995');
+if (!defined("MYSQL_ORDER_DATABASE")) define("MYSQL_ORDER_DATABASE", 'CONGRUENCY_ORDER');
+if (!defined("ORDER_LOGIN")) define("ORDER_LOGIN", 'orderer');
+if (!defined('ORDER_PASSWORD')) define('ORDER_PASSWORD', 'order995');
 
-define("MYSQL_FORM_DATABASE", 'CONGRUENCY_FORM');
-define("FORM_LOGIN", 'forms');
-define('FORM_PASSWORD', 'form995');
+if (!defined("MYSQL_FORM_DATABASE")) define("MYSQL_FORM_DATABASE", 'CONGRUENCY_FORM');
+if (!defined("FORM_LOGIN")) define("FORM_LOGIN", 'forms');
+if (!defined('FORM_PASSWORD')) define('FORM_PASSWORD', 'form995');
 
 /*
 
@@ -53,7 +53,7 @@ MYSQL Login info.
 */
 
 //define("SLASH", "\\");	//on win-dowz 
-define("SLASH", "/");	//on 'nix systems
+if (!defined("SLASH")) define("SLASH", "/");	//on 'nix systems
 /*
 SLASH refers to the slash between directories. The String "\\" will escape 
 to the '\' character, which denotes a directory. C:\STUFF\file.php (Stuff is a
@@ -70,7 +70,7 @@ So: /STUFF/file.php is the unix equivellent to the example above.
 
 
 //define("PLATFORMROOT", "c:".SLASH);	//win-dowz you may need to change the drive letter
-define("PLATFORMROOT", SLASH);	//on 'nix
+if (!defined("PLATFORMROOT")) define("PLATFORMROOT", SLASH);	//on 'nix
 
 /*
 	Windows the root of a drive is designated as a letter [a-z]:
@@ -82,7 +82,7 @@ define("PLATFORMROOT", SLASH);	//on 'nix
 
 */
 
-define("ABS_PATH", str_replace(chr(92), "/", dirname(__DIR__)) . "/");
+if (!defined("ABS_PATH")) define("ABS_PATH", str_replace(chr(92), "/", dirname(__DIR__)) . "/");
 
 //define("ABS_PATH", PLATFORMROOT.'web'.SLASH.'web'.SLASH.'congruency'.SLASH);
 /*
@@ -96,7 +96,7 @@ and come back once you understand those constants.
 */
 
 
-define("TAGS_DIR", ABS_PATH.'invocators'.SLASH.'tags'.SLASH);
+if (!defined("TAGS_DIR")) define("TAGS_DIR", ABS_PATH.'invocators'.SLASH.'tags'.SLASH);
 /*
 
 Pages can make calls to embedded objects, here is the sub directory that will contain 
@@ -105,7 +105,7 @@ the class files. Relative to the root of the install.
 */
 
 
-define("CONTENT_DIR", ABS_PATH.'content'.SLASH.'content'.SLASH);
+if (!defined("CONTENT_DIR")) define("CONTENT_DIR", ABS_PATH.'content'.SLASH.'content'.SLASH);
 /*
 
 Pages can make calls to embedded objects, here is the sub directory that will contain 
@@ -119,7 +119,7 @@ the class files. Relative to the root of the install.
 
 
 
-define("WORKING_PAGE", 'WORKING_PAGE');
+if (!defined("WORKING_PAGE")) define("WORKING_PAGE", 'WORKING_PAGE');
 /*
 
 WORKING_PAGE: 
@@ -130,9 +130,9 @@ For example: $_SESSION[WORKING_PAGE] yields the current working page object.
 
 */
 
-define("CLASS_LOADER_DIR",ABS_PATH."lib".SLASH."ClassLoader".SLASH);
+if (!defined("CLASS_LOADER_DIR")) define("CLASS_LOADER_DIR",ABS_PATH."lib".SLASH."ClassLoader".SLASH);
 
-define("CLASS_LOADER_HEADER",ABS_PATH."lib".SLASH."ClassLoader".SLASH."ClassLoaderHeader.php");
+if (!defined("CLASS_LOADER_HEADER")) define("CLASS_LOADER_HEADER",ABS_PATH."lib".SLASH."ClassLoader".SLASH."ClassLoaderHeader.php");
 /*
 
 CLASS_LOADER_HEADER
@@ -143,9 +143,9 @@ will need to modify this constant.
 */
 
 
-define("ETC",ABS_PATH."etc".SLASH);   // referenced by UserPrivilegeSet (require_once ETC."Privilege.php") but previously undefined
+if (!defined("ETC")) define("ETC",ABS_PATH."etc".SLASH);   // referenced by UserPrivilegeSet (require_once ETC."Privilege.php") but previously undefined
 
-define("LIB",ABS_PATH."lib");
+if (!defined("LIB")) define("LIB",ABS_PATH."lib");
 /*
 
 LIB
@@ -154,7 +154,7 @@ The library functions.
 */
 
 
-define("BIN",ABS_PATH."bin".SLASH);
+if (!defined("BIN")) define("BIN",ABS_PATH."bin".SLASH);
 /*
 
 BIN
@@ -163,7 +163,7 @@ Path to executable scripts.
 */
 
 
-define("HARNESS",BIN . "Harness".SLASH);
+if (!defined("HARNESS")) define("HARNESS",BIN . "Harness".SLASH);
 /*
 
 HARNESS
@@ -177,14 +177,14 @@ Path to test harnesses.
 
 
 
-define("KEY_PREFIX","<<<");	
-define("KEY_SUFFIX",">>>");
-define("TAG_KEY_PREFIX","/<<<");  
-define("TAG_KEY_SUFFIX","\>>>/");
-define("FUNCTION_NAME","[a-zA-Z]+");
-define("FUNCTION_ARGUMENTS","(\([A-Za-z0-9]*\))*");
-define("FUNCTION_ARGUMENT","/\([A-Za-z0-9]*\)/");
-define("GET_TAG_IDENTIFIER","/([a-zA-Z0-9]+\s?(?=\(\s?[a-zA-Z0-9]*\s?\)))|(\s?[a-zA-Z0-9]+\s?)/");
+if (!defined("KEY_PREFIX")) define("KEY_PREFIX","<<<");	
+if (!defined("KEY_SUFFIX")) define("KEY_SUFFIX",">>>");
+if (!defined("TAG_KEY_PREFIX")) define("TAG_KEY_PREFIX","/<<<");  
+if (!defined("TAG_KEY_SUFFIX")) define("TAG_KEY_SUFFIX","\>>>/");
+if (!defined("FUNCTION_NAME")) define("FUNCTION_NAME","[a-zA-Z]+");
+if (!defined("FUNCTION_ARGUMENTS")) define("FUNCTION_ARGUMENTS","(\([A-Za-z0-9]*\))*");
+if (!defined("FUNCTION_ARGUMENT")) define("FUNCTION_ARGUMENT","/\([A-Za-z0-9]*\)/");
+if (!defined("GET_TAG_IDENTIFIER")) define("GET_TAG_IDENTIFIER","/([a-zA-Z0-9]+\s?(?=\(\s?[a-zA-Z0-9]*\s?\)))|(\s?[a-zA-Z0-9]+\s?)/");
 
 
 
@@ -205,25 +205,25 @@ This is the subject header for that email
 ****************************************************************/
 
 
-define("ORDER_SUBJECT_HEADER","PAW: You have a new order");
+if (!defined("ORDER_SUBJECT_HEADER")) define("ORDER_SUBJECT_HEADER","PAW: You have a new order");
 
 /******************************************************************************
 The recipiants of that email.
 ****************************************************************/
-define("EMAIL_RECIPIANTS","steven.peterson@shaw.ca");
+if (!defined("EMAIL_RECIPIANTS")) define("EMAIL_RECIPIANTS","steven.peterson@shaw.ca");
 
 
 
 /**************************************************************
 Conditional Logging.
 ***********************************************************/
-define("USELOG_DEBUG",TRUE);
-define("USELOG_DATABASE",TRUE);
-define("USELOG_SECURITY",TRUE);
+if (!defined("USELOG_DEBUG")) define("USELOG_DEBUG",TRUE);
+if (!defined("USELOG_DATABASE")) define("USELOG_DATABASE",TRUE);
+if (!defined("USELOG_SECURITY")) define("USELOG_SECURITY",TRUE);
 
-define("LOGUSER_SERVER", 'localhost');
-define("LOGUSER_LOGIN", 'logUser');
-define("LOGUSER_PASSWORD", 'logloglog');
+if (!defined("LOGUSER_SERVER")) define("LOGUSER_SERVER", 'localhost');
+if (!defined("LOGUSER_LOGIN")) define("LOGUSER_LOGIN", 'logUser');
+if (!defined("LOGUSER_PASSWORD")) define("LOGUSER_PASSWORD", 'logloglog');
 
 
 ?>
