2f05ae3f2e6ff23dafbabb47d56e529e6d5fa368
ratchet <ratchet@congruency.local>
2026-07-06 00:30:55 -0600

    crank #21: retire dormant www/ + etc/ Constants.php; configure.php is the sole constants source

--- checkouts/current/www/index.php
+++ checkouts/current/www/index.php
@@ -4,7 +4,7 @@ Copyright (C) 2006 Steven Peterson
 Congruency is free software, licensed under the GNU GPLv2 or later.
 See the LICENSE file in the project root for full license terms.
 */
-require_once("Constants.php");
+require_once(__DIR__ . "/../boot/configure.php");
 require_once(BIN."Execute.php");
 ?>
 
